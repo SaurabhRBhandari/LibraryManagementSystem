fee0a9d2a80a9a10bea44671e52919d1fbe7c3a9
SaurabhRBhandari <bhandarisourabhrahul@gmail.com>
2022-01-12 15:52:26 +0530

    Added Book model,admin can now add/remove books

--- libmansys/models.py
+++ libmansys/models.py
@@ -3,7 +3,11 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
+    publisher=models.CharField(max_length=200,default='Not Available')
     isbn = models.PositiveIntegerField()
-    category = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50,default='Not Available')
+    summary = models.CharField(max_length=200,default='Not Available')
+    location = models.CharField(max_length=200,default='Not Available')
+    availability = models.IntegerField(default=0)
     def __str__(self):
         return str(self.name) + " ["+str(self.isbn)+']'
