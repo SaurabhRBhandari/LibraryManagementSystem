39f34d1f287bad0d5f17b425cf358149b9594a6f
SaurabhRBhandari <bhandarisourabhrahul@gmail.com>
2022-01-12 15:06:40 +0530

    Some minor login bugs resolved

--- LibManagement/urls.py
+++ LibManagement/urls.py
@@ -7,7 +7,8 @@ from views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/google/login/callback/libmansys/home',include('libmansys.urls')),
     path('accounts/', include('allauth.urls')),
     path('', Home.as_view(), name='home'),
-    path('accounts/google/login/callback/libmansys/home',include('libmansys.urls'),name='libmansys/home')
+    
 ]
--- libmansys/views.py
+++ libmansys/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+def index(request):
+    return render(request,'libmansys/home.html')
 
-class Home(TemplateView):
-    template_name = 'home.html'
+#class Home(TemplateView):
+#    template_name = 'libmansys/home.html'
