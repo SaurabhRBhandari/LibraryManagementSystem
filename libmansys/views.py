d3a5cd021116c83ead7b5c0bc41b60f41b6b22c6
SaurabhRBhandari <bhandarisourabhrahul@gmail.com>
2022-01-13 18:01:09 +0530

    Added Bootstrap

--- libmansys/views.py
+++ libmansys/views.py
@@ -7,14 +7,14 @@ from .forms import NewStudentForm
 
 def user_home(request):
     # TODO:Check this view properly
-    condition = is_new_user(request)
+    is_new_user(request)
     student = Student.objects.get(user_ID=request.user.id)
     if(request.method == 'POST'):
         s_form = NewStudentForm(request.POST, instance=student)
         if s_form.is_valid:
             s_form.save()
         return render(request, 'libmansys/user_home.html')
-    elif(condition):
+    elif(is_new_user):
         s_form = NewStudentForm(instance=student)
         context = {'s_form': s_form}
         return render(request, 'libmansys/update_user_profile.html', context)
